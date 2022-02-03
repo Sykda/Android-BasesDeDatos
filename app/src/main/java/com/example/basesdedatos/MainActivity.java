e791861154dea326a30edba65b934bc5e0dea4ae
Sykda <93369567+Sykda@users.noreply.github.com>
2022-02-03 13:58:43 +0100

    commit

--- app/src/main/java/com/example/basesdedatos/MainActivity.java
+++ app/src/main/java/com/example/basesdedatos/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.basesdedatos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et_codigo, et_descripcion, et_precio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_codigo=findViewById(R.id.txt_codigo);
+        et_descripcion=findViewById(R.id.txt_descripcion);
+        et_precio=findViewById(R.id.txt_precio);
+    }
+
+    public void registrar(View view){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion", null, 1);
+        SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
+
+        String codigo = et_codigo.getText().toString();
+        String descripcion = et_descripcion.getText().toString();
+        String precio = et_precio.getText().toString();
+
+        if(!codigo.isEmpty() && descripcion.isEmpty() && precio.isEmpty()){
+            ContentValues registro = new ContentValues();
+            registro.put("codigo", codigo);
+            registro.put("descripcion", descripcion);
+            registro.put("precio", precio);
+
+            BaseDeDatos.insert("articulos", null,registro);
+            BaseDeDatos.close();
+            et_codigo.setText("");
+            et_descripcion.setText("");
+            et_precio.setText("");
+
+            Toast.makeText(this, "Se ha registrado correctamente", Toast.LENGTH_SHORT).show();
+
+        }else{
+            Toast.makeText(this, "Debes rellenar todos los capos", Toast.LENGTH_SHORT).show();
+
+        }
+
     }
 }
